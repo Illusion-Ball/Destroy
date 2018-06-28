36d2f07f9ad2cfc3821caca17592f14701c6c627
Zirak Mondegari <codewizduo@gmail.com>
2018-06-28 21:21:28 +0530

    Update 0001.cpp

--- June/0001.cpp
+++ June/0001.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> occurred;
+        unordered_map<int, int> indexOfOccurrence;
         for (int i = 0; i < nums.size(); i++) {
-            int posOccurred = occurred[target - nums[i]];
-            if (posOccurred >= 1) {
-                return {posOccurred - 1, i};
+            if (indexOfOccurrence.count(target - nums[i]) > 0) {
+                return {indexOfOccurrence[target - nums[i]], i};
             }
-            occurred[nums[i]] = i + 1;
+            indexOfOccurrence[nums[i]] = i;
         }
     }
 };
