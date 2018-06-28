9f4e19e9fe200f740323a051bb5832c0b664028e
Zirak Mondegari <codewizduo@gmail.com>
2018-06-28 22:56:01 +0530

    Update 0007.cpp

--- June/0007.cpp
+++ June/0007.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int reverse(int num) {
-        if (num == INT_MIN) {
-            return 0;
-        }
-        int sign = (num < 0) ? -1 : 1;
         int reversedNum = 0;
-        num = abs(num);
         while (num) {
-            int numLimit = INT_MAX / 10;
-            if (reversedNum < numLimit || (reversedNum == numLimit && (num % 10) <= INT_MAX - numLimit * 10)) {
-                reversedNum = reversedNum * 10 + (num % 10);
-            } else {
+            if (reversedNum > INT_MAX / 10 || (reversedNum == INT_MAX / 10 && (num % 10) > 7)) {
                 return 0;
             }
+            if (reversedNum < INT_MIN / 10 || (reversedNum == INT_MIN / 10 && (num % 10) < -8)) {
+                return 0;
+            }
+            reversedNum = reversedNum * 10 + (num % 10);
             num /= 10;
         }
-        return sign * reversedNum;
+        return reversedNum;
     }
 };
